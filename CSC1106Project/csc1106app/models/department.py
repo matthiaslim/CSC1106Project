1baee6a7f54683f639e985b3c5858d43bfe62e16
Edwin <hewzhongxuan6f@gmail.com>
2024-06-14 13:07:26 +0800

    added in a description for department

--- CSC1106Project/csc1106app/models/department.py
+++ CSC1106Project/csc1106app/models/department.py
@@ -3,3 +3,6 @@ from django.db import models
 class Department(models.Model):
     department_id = models.AutoField(primary_key=True)
     department_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.department_name
