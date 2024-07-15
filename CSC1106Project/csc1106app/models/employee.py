e0677d31b5cdac2fe6a5ddf2e17db16d6ca6a79b
Isaiah-Ang <isaiah.ang@gmail.com>
2024-07-15 17:55:12 +0800

    add onboarded field to employee table

--- CSC1106Project/csc1106app/models/employee.py
+++ CSC1106Project/csc1106app/models/employee.py
@@ -17,6 +17,7 @@ class Employee(models.Model):
     contract_expiry_date = models.DateField(null=True, blank=True)
     employee_role = models.CharField(max_length=50)
     image = models.ImageField(upload_to='employee_images/', null=True, blank=True)
+    onboarded = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
