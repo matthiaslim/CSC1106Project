a6cb9bd242b6d8777e71d5ba5c2f48995f5799b4
Shun Hao <teoshunhao@gmail.com>
2024-07-15 15:22:01 +0800

    Removed Attendance_Date

--- CSC1106Project/csc1106app/models/attendance.py
+++ CSC1106Project/csc1106app/models/attendance.py
@@ -4,7 +4,6 @@ from .employee import Employee
 class Attendance(models.Model):
     attendance_id = models.AutoField(primary_key=True)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    attendance_date = models.DateField()
     time_in = models.DateTimeField()
     time_out = models.DateTimeField(null=True, blank=True)
     image = models.ImageField(upload_to='attendance_images/', null=True, blank=True)
