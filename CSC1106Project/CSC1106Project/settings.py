f9b696cd8290ac4d9e6472101743d0b0bcbdb901
txh <2303404@sit.singaporetech.edu.sg>
2024-07-16 14:31:28 +0800

    Auto stash before checking out "origin/dev/feature-merge"

--- CSC1106Project/CSC1106Project/settings.py
+++ CSC1106Project/CSC1106Project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'csc1106app',
     'widget_tweaks'
 ]
--- CSC1106Project/csc1106app/models/invoice.py
+++ CSC1106Project/csc1106app/models/invoice.py
@@ -1,12 +1,26 @@
 from django.db import models
 from .employee import Employee
+from datetime import date
 
 
 class Invoice(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        ('Rejected', 'Rejected')
+    ]
+    PAYMENT_CHOICES = [
+        ('Card', 'Card'),
+        ('Cash', 'Cash'),
+        ('Cheque', 'Cheque')
+    ]
+    
     invoice_id = models.AutoField(primary_key=True)
     employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
     invoice_date = models.DateField()
-    status = models.CharField(max_length=50)
+    payment_due_date = models.DateField(default=date.today)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    payment_terms = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='Card')
 
     def __str__(self):
         return self.invoice_id
