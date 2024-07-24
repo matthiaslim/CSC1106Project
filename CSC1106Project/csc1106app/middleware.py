a5467dbb0ab63e6164ea90909c39010efdee67d1
Matthias Lim <matthias.limeexuan@gmail.com>
2024-07-24 16:17:56 +0800

    session timeout modal

--- CSC1106Project/csc1106app/middleware.py
+++ CSC1106Project/csc1106app/middleware.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from .models import UserSession
 from django.contrib import messages
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 User = get_user_model()
 
@@ -18,37 +18,36 @@ class XFrameOptionsMiddleware():
         if request.path.startswith('/media/invoices/') or request.path.startswith('/media/sales/'):
             response['X-Frame-Options'] = 'SAMEORIGIN'  # Or comment out to remove the header
         return response
-    
+
+
 class SingleSessionMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.session.session_invalid = False
 
         if request.user.is_authenticated:
-            current_session_id = request.session.session_key 
-            
+            current_session_id = request.session.session_key
+
             user_session, created = UserSession.objects.get_or_create(user=request.user)
-            
+
             try:
                 existing_session_exists = Session.objects.filter(session_key=user_session.session_id).exists()
                 current_session = Session.objects.get(session_key=current_session_id)
             except Session.DoesNotExist:
                 request.session.session_invalid = True
-                return 
+                return
 
             if created or user_session.expiry_date < timezone.now() or not existing_session_exists:
                 user_session.session_id = current_session.session_key
                 user_session.expiry_date = current_session.expire_date
                 user_session.save()
-                return 
-            
+                return
+
             if current_session_id != user_session.session_id:
                 current_session.delete()
                 request.session.session_invalid = True
 
-    def process_response(self, request, response = None):
+    def process_response(self, request, response=None):
         if request.session.session_invalid:
-            return render(request, 'login.html')
-        
+            request.session['session_invalid'] = True
+            return redirect('login')
         return response
-                    
-            
